547c95919e36cf38c2a0ef20c0fe9264588cb4d0
mesanchezmtz <67165863+mesanchezmtz@users.noreply.github.com>
2020-06-19 15:06:54 -0500

    carritocatalogopagos

--- Industrial-Tools/Models/DetalleCompraModel.cs
+++ Industrial-Tools/Models/DetalleCompraModel.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+//Atributos venta
 namespace Industrial_Tools.Models
 {
     public class DetalleCompraModel
